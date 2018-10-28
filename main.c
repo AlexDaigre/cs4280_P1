48a6280a7c74de11ef1f0fa039a46bdd55690d95
Alexander Daigre <alex@Alexanders-MacBook-Pro.local>
2018-10-28 02:53:37 -0500

    working with testfile 1, not sure beyond that, and output is not nessisarialy up to spec

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "token.h"
 #include "scanner.h"
 
 int main (int argc, char *argv[]) {
-    char* fileName = "code.c";
-    FILE* sourceFile = fopen(fileName, "r");
+    char* sourceFileName = "P1_test1.fs182";
+    // char* fileExtention = ".fs182";
+    if (argc == 2) {
+        // sourceFileName = argv[1];
+        // strcat(sourceFileName, fileExtention);
+    } else {
+        printf("A sourcefile is requred as an argument\n");
+        return 1;
+    }
+    
+    printf("Reading: %s\n", sourceFileName);
+
+    FILE* sourceFile = fopen(sourceFileName, "r");
+    if (sourceFile == NULL){
+        printf("Could not open %s: %d\n", sourceFileName, errno);
+    }
 
     struct token newToken;
     do {
         newToken = getNextToken(sourceFile);
+        printf("MAIN:\n  Token ID: %d\n  Token Instance: %s\n  Line Number: %d\n", newToken.tokenId, newToken.tokenInstance, newToken.lineNum);
+    } while ( newToken.tokenId != 1003);
 
-    } while ( newToken.tokenId != eofTk);
+    fclose(sourceFile);
 }
--- scanner.c
+++ scanner.c
@@ -12,14 +12,16 @@ struct token getNextToken(FILE* fileName) {
     int nextState;
     struct token newToken;
     char tokenText[8];
+    int tokenTextLength = 0;
     while(state<1000) {
         if (nextChar == '\n'){
             lineNumber++;
         }
-        int stateNumber = charToColumn(nextChar);
-        nextState = fsaTable[state][stateNumber];
+        int charGroup = charToColumn(nextChar);
+        nextState = fsaTable[charGroup][state];
+        // printf("nextState: %d\n", nextState);
         if (nextState < 0){
-            printf("LEX ERROR at line %d", lineNumber);
+            printf("LEX ERROR at line %d\n", lineNumber);
             exit(1);
         }
         if (nextState >= 1000) {
@@ -27,28 +29,36 @@ struct token getNextToken(FILE* fileName) {
                 int i;
                 for (i = 0; i < keywordListSize; i++){
                     if (tokenText == keywordList[i]){
-                        newToken.tokenId = state;
+                        newToken.tokenId = nextState;
                         newToken.tokenInstance = tokenText;
                         newToken.lineNum = lineNumber;
+                        // printf("SCANNER:\n  Token ID: %d\n  Token Instance: %s\n  Line Number: %d\n", newToken.tokenId, newToken.tokenInstance, newToken.lineNum);
                         return newToken;
                     }
                 }
-                newToken.tokenId = state;
+                newToken.tokenId = nextState;
                 newToken.tokenInstance = tokenText;
                 newToken.lineNum = lineNumber;
+                // printf("SCANNER:\n  Token ID: %d\n  Token Instance: %s\n  Line Number: %d\n", newToken.tokenId, newToken.tokenInstance, newToken.lineNum);
                 return newToken;
             } else {
-                newToken.tokenId = state;
+                newToken.tokenId = nextState;
                 newToken.tokenInstance = tokenText;
                 newToken.lineNum = lineNumber;
+                // printf("SCANNER:\n  Token ID: %d\n  Token Instance: %s\n  Line Number: %d\n", newToken.tokenId, newToken.tokenInstance, newToken.lineNum);
                 return (newToken);
             }
         } else {
             state = nextState;
-            strcat(tokenText, nextChar);
+            // printf("nextChar: %c\n  tokenTextLength: %d\n", nextChar, tokenTextLength);
+            tokenText[tokenTextLength] = nextChar;
+            tokenTextLength++;
+            if (tokenTextLength > 8){
+                exit(1);
+            }
             nextChar = fgetc(fileName);
         }
     }
-    printf("LEX ERROR at line %d", lineNumber);
+    printf("LEX ERROR at line %d\n", lineNumber);
     exit(1);
 }
