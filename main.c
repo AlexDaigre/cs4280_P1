9323030554cc559ed7b72b3543ecaef8790d0cde
Alexander Daigre <alex@Alexanders-MacBook-Pro.local>
2018-10-28 21:39:58 -0500

    now takes the filename to open as a command line argument (without file extention)

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 #include "testScanner.h"
 
 int main (int argc, char *argv[]) {
-    char* sourceFileName = "P1_test4.fs182";
-    // char* fileExtention = ".fs182";
+    // char* sourceFileName = "P1_test4.fs182";
+    char sourceFileName[256] = "";;
+    char* fileExtention = ".fs182";
     if (argc == 2) {
-        // sourceFileName = argv[1];
-        // strcat(sourceFileName, fileExtention);
+        strcpy(sourceFileName, argv[1]);
+        strcat(sourceFileName, fileExtention);
     } else {
         printf("A sourcefile is requred as an argument\n");
         return 1;
     }
     
-    // printf("Reading: %s\n", sourceFileName);
+    printf("Reading: %s\n", sourceFileName);
 
     FILE* sourceFile = fopen(sourceFileName, "r");
     if (sourceFile == NULL){
