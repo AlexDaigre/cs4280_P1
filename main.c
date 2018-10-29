1547868a67f0b4896941997f23b7e0ae6742445b
Alexander Daigre <alex@Alexanders-MacBook-Pro.local>
2018-10-28 21:54:11 -0500

    now accepts either info from stdin or a file

--- main.c
+++ main.c
@@ -4,25 +4,33 @@
 #include <errno.h>
 #include "testScanner.h"
 
-int main (int argc, char *argv[]) {
+void invokeScannerFromFile(char* fileName);
+
+int main (int argc, char* argv[]) {
+    if (argc == 2) {
+        invokeScannerFromFile(argv[1]);
+    } else if (argc == 1){
+        testScanner(stdin);
+    } else {
+        printf("A sourcefile is requred as an argument. No additional arguments are allowed.\n");
+        return 1;
+    }
+}
+
+void invokeScannerFromFile(char* fileName){
     // char* sourceFileName = "P1_test4.fs182";
     char sourceFileName[256] = "";;
     char* fileExtention = ".fs182";
-    if (argc == 2) {
-        strcpy(sourceFileName, argv[1]);
-        strcat(sourceFileName, fileExtention);
-    } else {
-        printf("A sourcefile is requred as an argument\n");
-        return 1;
-    }
     
-    printf("Reading: %s\n", sourceFileName);
+    strcpy(sourceFileName, fileName);
+    strcat(sourceFileName, fileExtention);
 
     FILE* sourceFile = fopen(sourceFileName, "r");
     if (sourceFile == NULL){
         printf("Could not open %s: %d\n", sourceFileName, errno);
     }
 
+    // printf("Reading: %s\n", sourceFileName);
     testScanner(sourceFile);
 
     fclose(sourceFile);
